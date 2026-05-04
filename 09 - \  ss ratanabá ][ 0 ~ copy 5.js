f68329d831cb8d631c3c7b55fe6174cd63612aed
Kayque Avelar <kayque.avelar@rainforest.tech>
2026-05-04 15:54:04 -0300

    update

--- 09 - \  ss ratanabá ][ 0 ~ copy 5.js
+++ 09 - \  ss ratanabá ][ 0 ~ copy 5.js
@@ -15,6 +15,7 @@ const FastlyAPIToken = "uhZtofOcNnzoH6F5-m0bzsLvCqIjzNFG"
 linha 1
 linha 2
 let PASSWORD = "#KJDJ#IO))09idfsojst1"
+let PASSWORD = "#1233390s9Ddask2&"
 linha4
 
 const FastlyAPIToken = "uhZtofOcNnzoH6F5-m0bzsLvCqIjzNFG"
