30a2386399ad3f16d23f67e188316562949b23b8
Kayque Avelar <kayque.avelar@rainforest.tech>
2026-05-22 07:53:55 -0300

    adiciona vulnerabilidaes

--- vulnerable.cs
+++ vulnerable.cs
@@ -14,6 +14,7 @@ using System.IO.Compression;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Builder;
 
 namespace VulnerableDotNet
 {
